72e13d17c54b06cd0b567a539888f9fe2b3452f6
Jared M. Peterson <jared@peteyinc.com>
2016-05-24 00:40:43 -0600

    angular

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,11 @@
 angular.module('friendsList').controller('mainCtrl', function($scope) {
+
   $scope.name = "Jared";
-  $scope.friends = ["Katie", "Isaac", "Matt", "Clayton"]
+
+  $scope.friends = ["Katie", "Isaac", "Matt", "Clayton"];
+
+  $scope.addFriend = function() {
+    $scope.friends.push($scope.newFriend);
+    $scope.newFriend = "";
+  }
 });
